710723ce64ca4500af53a4b14896643086a6dd7c
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-01-21 10:47:17 +0200

    Completed App4

---
+++ App4/App4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(apskaiciuotiKMI(64, 1.80));
+            Console.Read();
+        }
+
+        public static double apskaiciuotiKMI(int mase,double ugis)
+        {
+            return mase / Math.Pow(ugis, 2);
+        }
+    }
+}
