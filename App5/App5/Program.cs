b4783ac2264813d99aa310665173875e4c755950
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-01-21 10:56:36 +0200

    Completed App5

--- App5/App5/Program.cs
+++ App5/App5/Program.cs
@@ -10,6 +10,22 @@ namespace App5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite teksta");
+            String tekstas = Console.ReadLine();
+            int tarpuEiluteje = surastiTarpus(tekstas);
+            Console.WriteLine(tarpuEiluteje);
+            Console.Read();
+        }
+
+        public static int surastiTarpus (String a)
+        {
+            int tarpai = 0;
+            foreach(char letter in a)
+            {
+                if (letter == ' ')
+                    tarpai++;
+            }
+            return tarpai;
         }
     }
 }
