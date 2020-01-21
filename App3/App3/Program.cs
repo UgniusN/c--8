fa13112e86d69816d4722ffe3988e125f5765f75
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-01-21 10:40:17 +0200

    Completed App3

---
+++ App3/App3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            groteles(5);
+            Console.Read();
+        }
+
+
+        public static void groteles(int kiekis)
+        {
+            for(int i=0; i<kiekis; i++)
+                Console.Write("#");
+        }
+    }
+}
