09560db2bbeae60b456e5f2ec741e58ade518975
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-01-21 11:04:54 +0200

    Completed App7

---
+++ App7/App7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pakeltasSkaicius = pakeltiSkaiciu(5);
+            Console.WriteLine(pakeltasSkaicius);
+            Console.Read();
+        }
+
+
+        public static double pakeltiSkaiciu(double a)
+        {
+            return Math.Pow(a, 2);
+        }
+    }
+}
