ed8a6afbe24167fa947ccbe038ac278ecf2a89b9
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-01-21 11:01:28 +0200

    App6

--- App6/App6/Program.cs
+++ App6/App6/Program.cs
@@ -10,6 +10,19 @@ namespace App6
     {
         static void Main(string[] args)
         {
+            int[] skaiciai = { 5, 4, 3, 6, 7, 8 };
+            int skaiciuSuma = grazintiMasyvoSuma(skaiciai);
+            Console.WriteLine(skaiciuSuma);
+            Console.Read();
+
+        }
+
+        public static int grazintiMasyvoSuma(int []a)
+        {
+            int suma = 0;
+            for (int i = 0; i < a.Length; i++)
+                suma += a[i];
+            return suma;
         }
     }
 }
