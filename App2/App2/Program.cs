a0c2391962161db3edb2298ab56d6c03cbda96e7
UgniusN <39160248+UgniusN@users.noreply.github.com>
2020-01-21 10:37:30 +0200

    Completed App2

---
+++ App2/App2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String [] vardai = {"Ugnius","Audrius", "Nojus", "Benas", "Jonas", "Rimas", "Nelsonas", "Gvidas"};
+            spausdintiVarda(5, vardai);
+            Console.Read();
+        }
+
+
+        public static void spausdintiVarda(int a,String [] b)
+        {
+            Random rand = new Random();
+            int vardas = rand.Next(1, b.Length);
+            for (int i=0; i<a; i++)
+            {
+                Console.WriteLine(b[vardas]);
+            }
+
+        }
+    }
+}
